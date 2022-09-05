30c67d78ac106352f654300680df637303a82dad
theochiu2010 <alanchiu3730@gmail.com>
2022-09-05 17:26:47 +0800

    update monthly interest to divide by 12

--- java/src/main/java/org/ikigaidigital/TimeDepositCalculator.java
+++ java/src/main/java/org/ikigaidigital/TimeDepositCalculator.java
@@ -10,16 +10,16 @@ public class TimeDepositCalculator {
             if (xs.get(i).days >= 30) {
                 if (xs.get(i).n == "student") {
                     if (xs.get(i).days < 366) {
-                        a += xs.get(i).b * 0.03;
+                        a += xs.get(i).b * 0.03 / 12;
                     }
                 } else if (xs.get(i).n == "premium") {
                     if (xs.get(i).days > 45) {
-                        a += xs.get(i).b * 0.05;
+                        a += xs.get(i).b * 0.05 / 12;
                     }
                 } else if (xs.get(i).n == "internal") {
                     a += xs.get(i).b * 0.1;
                 } else if (xs.get(i).n == "basic") {
-                    a += xs.get(i).b * 0.01;
+                    a += xs.get(i).b * 0.01 / 12;
                 }
             }
 
